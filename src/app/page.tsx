7488cb65213eb86b3f688ce53b7c2f0e702f8217
Antoine-Bottin <a.bottin.dev@gmail.com>
2025-02-19 16:20:54 +0100

    feat(portfolio): add snapscroll and picture

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,46 @@
 import './styles.scss'
+import Image from 'next/image'
+
+const interview = [
+  {
+    object: 'Who am I ?',
+    content: 'Patattieiteiteititietieiteit',
+  },
+  {
+    object: 'About me',
+    content: 'Patattieiteiteititietieiteit',
+  },
+  {
+    object: 'My favorite color',
+    content: 'Patattieiteiteititietieiteit',
+  },
+]
 
 const Page = () => {
-  return <div className="home-view">Home</div>
+  return (
+    <div className="home-view">
+      <div className="home-view__picture-section">
+        <div className="home-view__picture-section__picture">
+          <Image
+            src="/assets/Photo_CV.jpg"
+            alt="My picture"
+            fill
+            style={{ borderRadius: '50%' }}
+          />
+        </div>
+      </div>
+      <div className="infos-container">
+        {interview.map(({ object, content }) => {
+          return (
+            <div className="info" key={object}>
+              <h1>{object}</h1>
+              <div>{content}</div>
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
 }
 
 export default Page
