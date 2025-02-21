d094ef08330fe84f7d3f42420e17790f9a8c99da
Antoine-Bottin <a.bottin.dev@gmail.com>
2025-02-21 11:55:08 +0100

    feat(portfolio): added google map api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,70 @@
+'use client'
+
+import { ReactNode } from 'react'
+import { AdvancedMarker, APIProvider, Map } from '@vis.gl/react-google-maps'
+
 import './styles.scss'
 
-// interface IInterview {
-//     object: string
-//     content: ReactNode
-// }
+interface IInterview {
+    object: string
+    content: ReactNode
+}
 
-// const interview: IInterview[] = [
-//     {
-//         object: 'Who am I?',
-//         content: (
-//             <>
-//                 I&#39;m <span>Antoine</span>, an enthusiastic 40-year-old father
-//                 of two 👶🏼 and a software developer. <br /> Years ago, I used to
-//                 be a 🎻 (cello) player.
-//                 <br /> Then I became a 🍸🍻🎉 (a bartender, not an alcoholic
-//                 😉).
-//                 <br />
-//                 Four years ago, I decided to make a career change to become what
-//                 I really enjoy now.
-//             </>
-//         ),
-//     },
-//     {
-//         object: 'My favorite color',
-//         content: (
-//             <>
-//                 It&#39;s like a mix between blue and grey, but{' '}
-//                 <span>who cares?</span>
-//             </>
-//         ),
-//     },
-// ]
+const position = { lat: 49.922992, lng: 1.077483 }
+
+const interview: IInterview[] = [
+    {
+        object: 'Who am I?',
+        content: (
+            <>
+                I&#39;m <span>Antoine</span>, an enthusiastic 40-year-old father
+                of two 👶🏼 and a software developer. <br /> Years ago, I used to
+                be a 🎻 (cello) player.
+                <br /> Then I became a 🍸🍻🎉 (a bartender, not an alcoholic
+                😉).
+                <br />
+                Four years ago, I decided to make a career change to become what
+                I really enjoy now.
+            </>
+        ),
+    },
+    {
+        object: 'My favorite color',
+        content: (
+            <>
+                It&#39;s like a mix between blue and grey, but{' '}
+                <span>who cares?</span>
+            </>
+        ),
+    },
+    {
+        object: 'My living place ?',
+        content: (
+            <div className="google-map">
+                <APIProvider
+                    apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY!}
+                >
+                    <Map
+                        defaultCenter={position}
+                        defaultZoom={8}
+                        mapId="Dieppe"
+                        disableDefaultUI={true}
+                        onTilesLoaded={() => console.log('loaded')}
+                        fullscreenControl={true}
+                        colorScheme="DARK"
+                    >
+                        <AdvancedMarker position={position} />
+                    </Map>
+                </APIProvider>
+            </div>
+        ),
+    },
+]
 
 const Page = () => {
     return (
         <div className="home-view">
-            {/* <div className="infos-container">
+            <div className="infos-container">
                 {interview.map(({ object, content }, idx) => {
                     return (
                         <div key={idx} className="infos-container__snapscroll">
@@ -48,7 +77,7 @@ const Page = () => {
                         </div>
                     )
                 })}
-            </div> */}
+            </div>
         </div>
     )
 }
